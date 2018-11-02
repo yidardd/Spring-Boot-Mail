817c6ede7dba1c16c6fddbe87066a25d33d4a86f
rdd <13707259624@163.com>
2018-11-02 11:13:24 +0800

    测试上传展示问题

---
+++ src/test/java/net/rdd/testMail/MainTest.java
@@ -0,0 +1,14 @@
+package net.rdd.testMail;
+
+/**
+ * Created by rdd on 2018/11/2.
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(1111);
+
+    }
+
+}
